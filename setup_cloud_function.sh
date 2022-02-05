e1bd97028cd8250b9df1f6a665ad782d71c5671f
Anthony Sanchez <sanchezac@google.com>
2022-02-04 17:41:08 -0700

    updated cloud functions script and iam script

--- setup_cloud_function.sh
+++ setup_cloud_function.sh
@@ -13,14 +13,33 @@ gcloud auth login ${USER_EMAIL}
 echo "Set default project"
 gcloud config set project ${PROJECT_ID}
 
-echo "Deploying Cloud Function" 
+#get project number via project id to identify cloudbuild service account name
+PROJECT_NUM=`gcloud projects list \
+    --filter="$(gcloud config get-value project)" \
+    --format="value(PROJECT_NUMBER)"`
 
-gcloud functions deploy gcs-uri-to-pubsub \
+echo "Deploying Cloud Function docai-from-bucket-sync" 
+echo "this can take a few minutes..."
+
+gcloud functions deploy docai-from-bucket-sync \
 --region=${REGION} \
 --runtime python39 \
---trigger-resource gs://${PROJECT_ID}-csek-objects-bucket \
+--trigger-resource ${PROJECT_ID}_docai_source_bucket \
 --trigger-event google.storage.object.finalize \
---source ./functions/gcs_uri_to_pubsub/ \
+--source ./functions/cloud-function-docai-from-bucket-sync \
 --entry-point=main_func \
 --ingress-settings internal-only \
---set-env-vars TOPIC_NAME=${PS_TOPIC_NAME},PROJECT_ID=${PROJECT_ID}
+--set-env-vars PROJECT_ID=${PROJECT_ID},PROJECT_NUM=${PROJECT_NUM},LOCATION=us,PROCESSOR_ID=${DOCAI_PROCESSOR_ID},GCS_OUTPUT_URI=${PROJECT_ID}_docai_dest_bucket,SKIP_HITL=True
+
+echo "Deploying Cloud Function docai-from-bucket-sync" 
+echo "this can take a few minutes..."
+
+gcloud functions deploy docai-response-to-bq \
+--region=${REGION} \
+--runtime python39 \
+--trigger-resource ${PROJECT_ID}_docai_dest_bucket \
+--trigger-event google.storage.object.finalize \
+--source ./functions/cloud-function-docai-response-to-bq \
+--entry-point=main_func \
+--ingress-settings internal-only \
+--set-env-vars BQ_TABLE_ID=${PROJECT_ID}.docai.docai_results
--- setup_iam.sh
+++ setup_iam.sh
@@ -11,7 +11,16 @@ source ./config.sh
 gcloud auth login ${USER_EMAIL}
 echo "Assigning IAM Permissions"
 
+#get project number via project id to identify cloudbuild service account name
+PROJECT_NUM=`gcloud projects list \
+    --filter="$(gcloud config get-value project)" \
+    --format="value(PROJECT_NUMBER)"`
 
+#default cloud build account
+gcloud projects add-iam-policy-binding ${PROJECT_ID} \
+    --member=serviceAccount:${PROJECT_NUM}@cloudbuild.gserviceaccount.com \
+    --role=roles/cloudbuild.builds.builder
+    
 #default app engine service account
 #MUST BE RUN AFTER FUNCTION CREATION
 gcloud projects add-iam-policy-binding ${PROJECT_ID} \
